bceb9887f966ad74a9d6b50a26cb25da435e14a9
shadowue@hotmail.com <shadowue@hotmail.com>
2020-05-03 10:19:40 +0200

    Added NYT Service class

--- NYTimesSearch/Controllers/UserSearchController.cs
+++ NYTimesSearch/Controllers/UserSearchController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using NYTimesSearch.Services;
 
 namespace NYTimesSearch.Controllers
 {
@@ -15,9 +16,11 @@ namespace NYTimesSearch.Controllers
     [Authorize]
     public class UserSearchController : Controller
     {
-        private const string APIKEY = "zxZ1fZAhKIikmEedwq2Z38h29VeQudNo";
-        private const string URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
-        private readonly HttpClient client = new HttpClient();
+        NYTimesService nytService;
+        public UserSearchController()
+        {
+            nytService = new NYTimesService();
+        }
         // GET: UserSearch
         public ActionResult Index()
         {            
@@ -28,53 +31,11 @@ namespace NYTimesSearch.Controllers
         public async Task<ActionResult> SearchNews(SearchResults itemToSearch)
         {
             //SearchResults result = await SearchNYT(itemToSearch.SearchItem);
-            SearchResults res = await SearchNYT(itemToSearch.SearchItem, itemToSearch.Page);
+            SearchResults res = await nytService.SearchNYT(itemToSearch.SearchItem, itemToSearch.Page);
             // return Content("Searching item: " + res);
             return View("SearchResults", res);
         }
 
-        private async Task<SearchResults> SearchNYT(string keywords, string page)
-        {
-            SearchResults result = new SearchResults();
-            try
-            {
-                var builder = new UriBuilder(URL);
-                var query = HttpUtility.ParseQueryString(builder.Query);
-                query["api-key"] = APIKEY;
-                query["page"] = page;
-                query["q"] = keywords;
-                builder.Query = query.ToString();
-                string url = builder.ToString();
-
-                //HttpResponseMessage response = await client.GetAsync(url);
-                //response.EnsureSuccessStatusCode();
-                //string responseBody = await response.Content.ReadAsStringAsync();
-                // Above three lines can be replaced with new helper method below
-                string responseBody = await client.GetStringAsync(url);
-
-                dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
-
-                SearchResultItem item;
-                int itemsFound = jsonResponse.response.meta.hits > 10 ? 10 : jsonResponse.response.meta.hits;
-                for (int i = 1; i < itemsFound; i++)
-                {
-                    item = new SearchResultItem() { };
-                    item.ArticleName = jsonResponse.response.docs[i].headline.main;
-                    item.ArticleLink = jsonResponse.response.docs[i].web_url;
-                    item.ArticleSource = jsonResponse.response.docs[i].source;
-                   
-                    result.SearchResultsList.Add(item);
-                }
-
-                
-                return result;
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-                return null;
-            }
-        }
+        
     }
 }
---
+++ NYTimesSearch/Services/NYTimesService.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using NYTimesSearch.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NYTimesSearch.Services
+{
+    internal class NYTimesService
+    {
+        private const string APIKEY = "zxZ1fZAhKIikmEedwq2Z38h29VeQudNo";
+        private const string URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
+        private readonly HttpClient client;
+
+        public NYTimesService()
+        {
+            client = new HttpClient();
+        }
+        internal async Task<SearchResults> SearchNYT(string keywords, string page)
+        {
+            SearchResults result = new SearchResults();
+            try
+            {
+                var builder = new UriBuilder(URL);
+                var query = HttpUtility.ParseQueryString(builder.Query);
+                query["api-key"] = APIKEY;
+                query["page"] = page;
+                query["q"] = keywords;
+                builder.Query = query.ToString();
+                string url = builder.ToString();
+
+                //HttpResponseMessage response = await client.GetAsync(url);
+                //response.EnsureSuccessStatusCode();
+                //string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                string responseBody = await client.GetStringAsync(url);
+
+                dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
+
+                SearchResultItem item;
+                int itemsFound = jsonResponse.response.meta.hits > 10 ? 10 : jsonResponse.response.meta.hits;
+                for (int i = 1; i < itemsFound; i++)
+                {
+                    item = new SearchResultItem() { };
+                    item.ArticleName = jsonResponse.response.docs[i].headline.main;
+                    item.ArticleLink = jsonResponse.response.docs[i].web_url;
+                    item.ArticleSource = jsonResponse.response.docs[i].source;
+
+                    result.SearchResultsList.Add(item);
+                }
+
+
+                return result;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return null;
+            }
+        }
+    }
+}
