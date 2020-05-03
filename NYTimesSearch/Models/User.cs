d41ea076c30c7e304a6a079f8878d0bbc40f17ab
shadowue@hotmail.com <shadowue@hotmail.com>
2020-05-03 09:52:34 +0200

    Initial commit

---
+++ NYTimesSearch/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NYTimesSearch.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Search news")]
+        public string SearchItem { get; set; }
+    }
+}
